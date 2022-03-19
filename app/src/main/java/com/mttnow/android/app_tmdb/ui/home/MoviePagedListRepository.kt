818e8ed58a5695fcdb14053274ce776b4233be7b
PC <domashnv@mail.ru>
2022-03-19 22:15:31 +0300

    add MoviePagedListRepository  получает параметром TMDBInterface внутри 2 метода: -fetchLiveMoviePagedList - возвращает LiveData<PagedList<Movie>>  - getNetworkState() возвращает LiveData<NetworkState>

---
+++ app/src/main/java/com/mttnow/android/app_tmdb/ui/home/MoviePagedListRepository.kt
@@ -0,0 +1,36 @@
+package com.mttnow.android.app_tmdb.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.mttnow.android.app_tmdb.data.Const
+import com.mttnow.android.app_tmdb.data.apiNetwork.NetworkState
+import com.mttnow.android.app_tmdb.data.apiNetwork.TMDBInterface
+import com.mttnow.android.app_tmdb.data.repository.MovieDataSource
+import com.mttnow.android.app_tmdb.data.repository.MovieDataSourceFactory
+import com.mttnow.android.app_tmdb.modeldata.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MoviePagedListRepository (private val apiService : TMDBInterface) {
+
+    lateinit var moviePagedList: LiveData<PagedList<Movie>>
+    lateinit var moviesDataSourceFactory: MovieDataSourceFactory
+
+    fun fetchLiveMoviePagedList (compositeDisposable: CompositeDisposable) : LiveData<PagedList<Movie>> {
+        moviesDataSourceFactory = MovieDataSourceFactory(apiService, compositeDisposable)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPageSize(Const.POST_PER_PAGE)
+            .build()
+
+        moviePagedList = LivePagedListBuilder(moviesDataSourceFactory, config).build()
+        return moviePagedList
+    }
+
+    fun getNetworkState(): LiveData<NetworkState> {
+        return Transformations.switchMap<MovieDataSource, NetworkState>(
+            moviesDataSourceFactory.moviesLiveDataSource, MovieDataSource::networkState)
+    }
+}
