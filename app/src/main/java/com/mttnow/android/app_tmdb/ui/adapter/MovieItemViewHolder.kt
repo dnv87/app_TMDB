d73cb7a2136e4d98b9fb6038eca7704a1fda1929
Vadim Beskorovainyi <v.beskorovainyi@growapp.biz>
2022-03-31 19:26:41 +0300

    правка MovieItemViewHolder.kt

--- app/src/main/java/com/mttnow/android/app_tmdb/ui/adapter/MovieItemViewHolder.kt
+++ app/src/main/java/com/mttnow/android/app_tmdb/ui/adapter/MovieItemViewHolder.kt
@@ -8,33 +8,33 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mttnow.android.app_tmdb.R
 import com.mttnow.android.app_tmdb.data.Const
-import com.mttnow.android.app_tmdb.data.MAIN
 import com.mttnow.android.app_tmdb.modeldata.Movie
 
-class MovieItemViewHolder (view: View, val onMovieCkick: (Int)-> Unit) : RecyclerView.ViewHolder(view) {
+class MovieItemViewHolder(view: View, val onMovieCkick: (Int) -> Unit) :
+    RecyclerView.ViewHolder(view) {
+
+    //Всегда findViewById вызывать взаранеедля ViewHolder
+    private val tvMovieTitle = itemView.findViewById<TextView>(R.id.cv_movie_title)
+    private val tvMovieRelease = itemView.findViewById<TextView>(R.id.cv_movie_release_date)
+    private val ivMoviePoster = itemView.findViewById<ImageView>(R.id.cv_iv_movie_poster)
 
     fun bind(movie: Movie?) {
 
-        itemView.findViewById<TextView>(R.id.cv_movie_title).text = movie?.title
-        itemView.findViewById<TextView>(R.id.cv_movie_release_date).text =  movie?.releaseDate
+        tvMovieTitle.text = movie?.title
+        tvMovieRelease.text = movie?.releaseDate
 
         val moviePosterURL = Const.THE_MOVIES_DB_IMAGE_BASE_URL_WITH_SIZE342 + movie?.poster_path
         val into = Glide.with(itemView.context)
             .load(moviePosterURL)
-            .into(itemView.findViewById<ImageView>(R.id.cv_iv_movie_poster))
+            .into(ivMoviePoster)
 
-        itemView.setOnClickListener{
-
-            Log.d("my", "setOnClickListener ${movie!!.id}")
-
-/*            // бывает глючит
-            val action = MovieFragmentDirections.actionNavigationMovieToNavigationMovieDetail(movie!!.id)
-            MAIN.navControl.navigate(action)*/
-
-            onMovieCkick.invoke(movie!!.id)
-
+        itemView.setOnClickListener {
+            onClick(movie!!.id)
         }
     }
 
+    fun onClick(movieId: Int) {
+        onMovieCkick.invoke(movieId)
+    }
 
 }
